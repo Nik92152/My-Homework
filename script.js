9f6fbc216f6a161c98bd9b8d73261127552bed2b
Nik92152 <extend52@yandex.ru>
2022-11-08 21:59:58 +0300

    Добавил файл с заданием от 1-го занятия

---
+++ script.js
@@ -0,0 +1,48 @@
+console.log("Hello world");
+let number=23;
+console.log(number);
+console.log(typeof number);
+let Boolean= true;
+console.log(Boolean);
+console.log(typeof Boolean);
+let text="True";
+console.log(text);
+console.log(typeof text);
+
+let noting=true;
+console.log(noting);
+console.log(typeof noting);
+let Symbol=("$");
+console.log(Symbol);
+console.log(typeof Symbol);
+let bigiant=121616162132132165161616161565165161n;
+console.log(bigiant);
+console.log(typeof bigiant);
+
+/*const random_number=33;
+random_number=34;
+console.log(random_number);*/
+
+let user1 = {
+    name: "Tom",
+    age: 23
+}
+
+var user2 = {
+    name: "Den",
+    age: 25
+}
+
+const user3 = {
+    name: "Roy",
+    age: 19
+}
+
+user1=user2;
+console.log(user1)
+
+user2=user1;
+console.log(user2)
+
+user3=user1;
+console.log(user3)
