64ce2414834a1c7c1610a032a25f88428d6d0b55
Nik92152 <extend52@yandex.ru>
2022-11-15 21:44:25 +0300

    Задачи из практики(кроме последней)

---
+++ HW-19.11.js
@@ -0,0 +1,46 @@
+// Задание 2 Практика
+// function increase(number,degree=3){
+   
+//     result= Math.pow(number,degree);
+//     return result;
+    
+// }
+
+// console.log(increase(2));
+
+// Задание с пары 1
+// function omg(name = "admin"){
+//     console.log(`Привет ${name}`);
+// }
+
+// omg("Димар")
+// omg()
+
+// Вывод числа n раз
+// const repeat=(text,count) => {
+//     for (let i=0; i < count; i++) {
+//         console.log(text);
+//     }
+// }
+
+// repeat("Привет",4);
+
+// Задача 2.
+// let stroke=("а, я, у, ю, о, е, ё, э, и, ы");
+
+
+
+// const letters=(text) => {
+//     if (stroke.indexOf(text)!== - 1){
+//         console.log("True");
+//     }
+//     else{
+//         console.log ("False");
+//     }
+// }
+
+// letters("ш")
+
+
+
+
