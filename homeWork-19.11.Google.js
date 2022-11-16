57dd97a4b31278793e085feade130d6cc3809295
Nik92152 <extend52@yandex.ru>
2022-11-16 15:42:37 +0300

    Задачи на 19.11 ИЗ ГУГЛА( кроме массивов.

---
+++ homeWork-19.11.Google.js
@@ -0,0 +1,150 @@
+// Task 1
+
+// node homeWork-19.11.Google.js
+
+// function func1(a){
+//     let z = a * a;
+//    console.log(z);  /* а return здесь обязателен?*/
+// } 
+// func1(4)
+
+// func2(10,57)
+
+// Task 2
+
+// function func2(a,b){
+//     return a+b;
+    
+// }
+// console.log(func2(2,7));
+
+// Task 3
+// function func3(a,b,c){
+//    return (a-b)/c;
+    
+// }
+
+// console.log(func3(5,1,2)); 
+
+// Task 4
+
+// function func4(a) {
+// switch(a) {
+//     case 1 : return "Понедельник";
+//     case 2 : return "Вторник";
+//     case 3 : return "Среда";
+//     case 4 : return "Четверг";
+//     case 5 : return "Пятница";
+//     case 6 : return "Суббота";
+//     case 7 : return "Воскресенье";
+//     default : return ("Вы ввели неправильное число, диапазон от 1 до 7");
+    
+
+    // case 2 : console.log("Вторник")
+    //     break; 
+    // case 3 : console.log("Среда")
+    //     break; 
+    // case 4 : console.log("Четверг")
+    //     break; 
+    // case 5 : console.log("Пятница") 
+    //     break; 
+    // case 6 : console.log("Суббота") 
+    //     break; 
+    // case 7 : console.log("Воскресенье") 
+    //     break; 
+    // default : console.log("Вы ввели неправильное число, диапазон от 1 до 7") 
+    //     break; 
+//     }     
+// }
+
+// console.log(func4(7));
+
+
+// Потренировался с if else
+    /* if (a == 1) {
+    console.log("Понедельник");
+    }
+    else if (a  == 2) {
+
+    console.log("Вторник");
+    }
+    else if (a  == 3) {
+
+    console.log("Среда");
+    }
+    else if (a  == 4) {
+
+    console.log("Четверг");
+    }
+    else if (a  == 5) {
+
+    console.log("Пятница");
+    }
+    else if (a  == 6) {
+
+    console.log("Суббота");
+    }
+    else if (a  == 7) {
+
+    console.log("Воскресенье");
+    } 
+    else{
+        console.log("Вы ввели неправильное число"); */
+
+
+
+// Task 5
+
+// function func5(a,b){
+//     let z =a+b;
+//     if (z > 10){
+//         return true;
+//     }
+//     else{
+        
+//         return false;
+//     }
+// }
+
+// console.log(func5(3,55)); 
+
+// // Task 6
+
+// function func6(a,b){
+//     if ((a+b) > 10) {
+//       return true;
+//     }
+//     else{
+//         return false;
+
+//     }
+// }
+
+// console.log(func6(5,1)); 
+
+// Task 7
+
+// function func7(a){
+//     if (a > 0) {
+//         return false;
+//     }
+//     else{
+//         return true;
+//     }
+// }
+
+// console.log(func7(5));
+
+// Task 8
+
+// function isNumberInRange(a){
+//     if  (0 < a && a < 10) {
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+// console.log(isNumberInRange(7));
+
